9278a070217b1e0167447f4bf2ea7dfad1802bbe
TahirSanadi <tahirsanadi14@gmail.com>
2024-08-14 18:36:12 +0530

    all fixed now when you reload browser the todo will be remain same

--- 10ToDocontextlocal/src/App.jsx
+++ 10ToDocontextlocal/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   useEffect(()=>{
     const todos = JSON.parse(localStorage.getItem("todos"))
-    if(todos && todos.lenght>0){
+    if(todos && todos.length>0){
       setTodos(todos)
     }
   }, [])
